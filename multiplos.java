4c30f93200da26c93d0fcfb8e5ecf3387a1bb791
Pablon03 <60820789+Pablon03@users.noreply.github.com>
2021-09-29 21:44:45 +0200

    Update multiplos.java

--- multiplos.java
+++ multiplos.java
@@ -8,11 +8,13 @@ class Main {
 
     // Output input by user
     System.out.println("El número introducido es: " + num );      
-    if (num % 2 == 0) {
-  System.out.println("EL número introducido es múltiplo de 2");
+    if (num % 2 == 0 && num % 3==0) {
+  System.out.println("El número introducido es múltiplo de 2 y de 3");
 } else {
         if (num % 3 == 0){
             System.out.println("El número es múltiplo de 3");
+        } else  if(num %2==0){
+            System.out.println("El número introducido es múltiplo de 2");
         } else {
             System.out.println("El número introducido no es múltiplo de 2 ni de 3");
         }
