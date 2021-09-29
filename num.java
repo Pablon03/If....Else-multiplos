6b9580176b34501362a64528af6511c616b8c8bd
Pablon03 <60820789+Pablon03@users.noreply.github.com>
2021-09-29 21:35:26 +0200

    Create num.java

---
+++ num.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+
+    // Numerical input
+    int num = myObj.nextInt();
+
+    // Output input by user
+    System.out.println("El número introducido es: " + num );      
+    if (num % 2 == 0) {
+  System.out.println("EL número introducido es múltiplo de 2");
+} else {
+        if (num % 3 == 0){
+            System.out.println("El número es múltiplo de 3");
+        } else {
+            System.out.println("El número introducido no es múltiplo de 2 ni de 3");
+        }
+} 
+  }
+}
